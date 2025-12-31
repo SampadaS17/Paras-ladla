adaef7d88609d8d6490912675c2ce98dc482648b
samspam_17 <111726151+SampadaS17@users.noreply.github.com>
2025-12-31 14:22:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+// const video = document.getElementById("heroVideo");
+// const btn = document.getElementById("playBtn");
+
+// btn.addEventListener("click", () => {
+//     video.volume = 0.2;
+//     video.muted = false;
+//     video.play();
+//     btn.style.display = "none"; // hide after play
+// });
+
+
+
+// function toggleMenu() {
+//     document.getElementById("navLinks").classList.toggle("show");
+// }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const slides = document.querySelectorAll(".about-slider .slide");
+    let index = 0;
+
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[0].classList.add("active");
+
+    setInterval(() => {
+        slides[index].classList.remove("active");
+        index = (index + 1) % slides.length;
+        slides[index].classList.add("active");
+    }, 2000);
+
+});
+
+
+function openJourney() {
+    const modal = document.getElementById("journeyModal");
+    if (modal) {
+        modal.style.display = "block";
+    } else {
+        console.error("Modal not found");
+    }
+}
+
+function closeJourney() {
+    const modal = document.getElementById("journeyModal");
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
